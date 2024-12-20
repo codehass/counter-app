71289a7a3b508ad0a75b70272e504b23dff0d131
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2024-12-20 12:07:11 +0100

    style app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';  
 import './App.css';
-import Counter from './Counter'
+import Counter from './components/Counter'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex items-center justify-center h-screen">
       <Counter />
     </div>
   );
